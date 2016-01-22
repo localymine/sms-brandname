7b8f1b557e472b289f4807c3dc38fdeea0efff37
Kantoh <leduongkhang@gmail.com>
2016-01-22 17:07:30 +0700

    sms marketing

--- public_html/wp-content/themes/smsbrand/page-sms-marketing.php
+++ public_html/wp-content/themes/smsbrand/page-sms-marketing.php
@@ -6,57 +6,88 @@
 get_header();
 ?>
 
-<!-- marketing black-->
-<div id="marketing-black">
-    <div class="container">
-        <div class="row-gap-big"></div>
-        <div class="row">
-            <div class="col-xs-12 col-md-6 wow fadeInLeft" data-wow-delay="0.5s">
-                <img class="img-responsive pull-left" src="images/slider-6.png"/>
+<?php
+$args = array(
+    'post_type' => array('sms'),
+    'post_per_page' => 1,
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'sms-type',
+            'field' => 'slug',
+            'terms' => array('sms-marketing'),
+        ),
+    ),
+);
+$loop = new WP_Query($args);
+if ($loop->have_posts()):
+    while ($loop->have_posts()): $loop->the_post();
+        $banner_image = get_field('banner_image');
+        ?>
+
+        <section>
+            <div class="col-xs-12 col-md-12">
+                <div class="row">
+                    <img src="<?php echo $banner_image['url'] ?>" />
+                </div>
             </div>
-            <div class="col-xs-12 col-md-6 text-left wow fadeInRight" data-wow-delay="0.5s">
-                <ul class="list_market">
-                    <li>
-                        <span class="txt_666">Demo demo demo demo demo demo demo demo demo demo demo</span>
-                    </li>
-                    <li>
-                        <span class="txt_666">Demo demo demo demo demo demo demo demo demo demo demo</span>
-                    </li>
-                    <li>
-                        <span class="txt_666">Demo demo demo demo demo demo demo demo demo demo demo</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="row-gap-big"></div>
-    </div>
-</div>
-<!-- marketing end -->
-<!-- marketing white-->
-<div id="marketing-white">
-    <div class="container">
-        <div class="row-gap-big"></div>
-        <div class="row">
-            <div class="col-xs-12 col-md-6 text-left wow fadeInLeft" data-wow-delay="0.5s">
-                <ul class="list_market">
-                    <li>
-                        <span class="txt_666">Demo demo demo demo demo demo demo demo demo demo demo</span>
-                    </li>
-                    <li>
-                        <span class="txt_666">Demo demo demo demo demo demo demo demo demo demo demo</span>
-                    </li>
-                    <li>
-                        <span class="txt_666">Demo demo demo demo demo demo demo demo demo demo demo</span>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-md-6 wow fadeInRight" data-wow-delay="0.5s">
-                <img class="img-responsive pull-left" src="images/slider-6.png"/>
-            </div>
-        </div>
-        <div class="row-gap-big"></div>
-    </div>
-</div>
-<!-- marketing end -->
+        </section>
+
+        <!-- content -->
+        <?php
+        $counter = 0;
+        //
+        if (have_rows('block_information')):
+            while (have_rows('block_information')): the_row();
+                $image = get_sub_field('image');
+                $class = ($counter % 2 == 0) ? 'even' : 'odd';
+                ?>
+                
+                <?php if(get_sub_field('set_position') == 0): ?>
+                    <div id="marketing-<?php the_ID() ?>" class="<?php echo $class ?>">
+                        <div class="container">
+                            <div class="row-gap-big"></div>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6 wow fadeInLeft" data-wow-delay="0.5s">
+                                    <img class="img-responsive pull-left" src="<?php echo $image['url'] ?>"/>
+                                </div>
+                                <div class="col-xs-12 col-md-6 text-left wow fadeInRight" data-wow-delay="0.5s">
+                                    <?php echo get_sub_field('content') ?>
+                                </div>
+                            </div>
+                            <div class="row-gap-big"></div>
+                        </div>
+                    </div>
+
+                <?php else: ?>
+
+                    <div id="marketing-<?php the_ID() ?>" class="<?php echo $class ?>">
+                        <div class="container">
+                            <div class="row-gap-big"></div>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6 wow fadeInLeft" data-wow-delay="0.5s">
+                                    <img class="img-responsive pull-left" src="<?php echo $image['url'] ?>"/>
+                                </div>
+                                <div class="col-xs-12 col-md-6 text-left wow fadeInRight" data-wow-delay="0.5s">
+                                    <?php echo get_sub_field('content') ?>
+                                </div>
+                            </div>
+                            <div class="row-gap-big"></div>
+                        </div>
+                    </div>
+
+                <?php endif; ?>
+
+                <?php
+                $counter++;
+            endwhile;
+        endif;
+        ?>
+        <!-- content end -->
+
+        <?php
+    endwhile;
+endif;
+wp_reset_postdata();
+?>
 
 <?php get_footer(); ?>
