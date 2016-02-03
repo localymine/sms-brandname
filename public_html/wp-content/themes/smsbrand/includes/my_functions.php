ec3214dd91cddf2cefd08e71295984fa5f48fefe
Kantoh <leduongkhang@gmail.com>
2016-02-04 01:11:24 +0700

    generate omw_guid

--- public_html/wp-content/themes/smsbrand/includes/my_functions.php
+++ public_html/wp-content/themes/smsbrand/includes/my_functions.php
@@ -329,3 +329,43 @@ function setPostViews($post_ID, $count_key = '') {
 
 // Remove issues with prefetching adding extra views
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+/* -------------------------------------------------------------------------- */
+#
+#   Taken from the PHP documentation website.
+#
+#   Kristof_Polleunis at yahoo dot com
+#
+#   A guid function that works in all php versions:
+#   MEM 3/30/2015 : Modified the function to allow someone
+#       to specify whether or not they want the curly
+#       braces on the GUID.
+#
+
+function omw_guid($opt = true, $convert_case = true) {       //  Set to true/false as your default way to do this.
+    if (function_exists('com_create_guid')) {
+        if ($opt) {
+            return com_create_guid();
+        } else {
+            return trim(com_create_guid(), '{}');
+        }
+    } else {
+        mt_srand((double) microtime() * 10000);    // optional for php 4.2.0 and up.
+        if ($convert_case) {
+            $charid = strtoupper(md5(uniqid(rand(), true)));
+        } else {
+            $charid = strtolower(md5(uniqid(rand(), true)));
+        }
+        $hyphen = chr(45);    // "-"
+        $left_curly = $opt ? chr(123) : "";     //  "{"
+        $right_curly = $opt ? chr(125) : "";    //  "}"
+        $uuid = $left_curly
+                . substr($charid, 0, 8) . $hyphen
+                . substr($charid, 8, 4) . $hyphen
+                . substr($charid, 12, 4) . $hyphen
+                . substr($charid, 16, 4) . $hyphen
+                . substr($charid, 20, 12)
+                . $right_curly;
+        return $uuid;
+    }
+}
